02080a91e1f32dd98fb40054e60eb8bcb3d9b5bc
Ahmad Javaid <ahmadjavaiddev@users.noreply.github.com>
2026-08-24 01:20:32 +0000

    Exercise verified registry dependencies

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import isNumber from "is-number";
+
 export function add(left, right) {
+  if (!isNumber(left) || !isNumber(right)) throw new TypeError("Both values must be numbers");
   return left + right;
 }
